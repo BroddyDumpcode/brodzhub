5af2072b319d959185ff8984465c74adaaf7183f
broddy <palpalehot69@gmail.com>
2026-03-10 03:13:13 +0700

    Update fly.lua

--- modules/fly.lua
+++ modules/fly.lua
@@ -77,38 +77,7 @@ local flyDragging = false
 local dragging = false
 
 -- slider knob events
-knob.InputBegan:Connect(function(input)
-    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-        dragging = true
-        flyDragging = true
-    end
-end)
-
-UserInputService.InputEnded:Connect(function(input)
-        if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
-            dragging = false
-            flyDragging = false
-        end
-    end)
-    
-    -- RenderStepped fly
-    flyConnection = RunService.RenderStepped:Connect(function()
-        if flyDragging then return end -- skip fly saat slider digeser
-    
-        if not flying then return end
-        local hrp = getHRP()
-        if not hrp then return end
-    
-        local move = Vector3.new(control.l + control.r, control.u + control.d, control.f + control.b)
-        local camCF = cam.CFrame
-    
-        bv.Velocity = camCF.LookVector * move.Z * speed +
-                      camCF.RightVector * move.X * speed +
-                      Vector3.new(0, move.Y * speed, 0)
-        if move.Magnitude > 0 then
-            bg.CFrame = CFrame.new(hrp.Position, hrp.Position + camCF.LookVector)
-        end
-    end)
 
 return fly
+
 
