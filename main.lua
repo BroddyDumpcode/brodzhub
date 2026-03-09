5a14467f2b439407ee81bd9694ba44f2a8a0211a
broddy <palpalehot69@gmail.com>
2026-03-09 16:29:45 +0700

    main.lua

---
+++ main.lua
@@ -0,0 +1,13 @@
+local repo = "https://raw.githubusercontent.com/BroddyDumpcode/"
+local gui = loadstring(game:HttpGet(repo..""))()
+
+local module = {
+    ngapung = loadstring(game:HttpGet(repo..""))()
+    ngabret = loadstring(game:HttpGet(repo..""))()
+    esp = loadstring(game:HttpGet(repo..""))()
+    pepet = loadstring(game:HttpGet(repo..""))()
+    nclip = loadstring(game:HttpGet(repo..""))()
+    infjmp = loadstring(game:HttpGet(repo..""))()
+}
+
+gui:Init(module)
