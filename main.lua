0a6a24b83fcd9cabba17a76a088210312e10d9e9
broddy <palpalehot69@gmail.com>
2026-03-11 02:41:44 +0700

    debug loader main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 local repo = "https://raw.githubusercontent.com/BroddyDumpcode/luah/main/"
 local GUI = loadstring(game:HttpGet(repo.."utils/GUI.lua"))()
+print(game:HttpGet(repo.."utils/GUI.lua"))
 local modules = {
     ngapung = loadstring(game:HttpGet(repo.."modules/fly.lua"))(),
     ngabret = loadstring(game:HttpGet(repo.."modules/ngabret.lua"))(),
@@ -8,8 +9,10 @@ local modules = {
     infjmp = loadstring(game:HttpGet(repo.."modules/infjmp.lua"))(),
     pepet = loadstring(game:HttpGet(repo.."utils/tpgui.lua"))()
 }
+
 print("feature has been loaded...")
 print("ngabret:", modules.ngabret)
 print("setSpeed:", modules.ngabret and modules.ngabret.setSpeed)
 --GUI:Init(modules)
+
 
